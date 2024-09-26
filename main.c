250a4d1dc46271f6b00ffb23e00c23c65cd1748e
jackren <sren0023@student.monash.edu>
2024-09-26 17:33:28 +1000

    Revert "test"
    This reverts commit 6401eea217a72e39c2eb39a933447a81ac0effdf.

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
-// variables must be declared before using
-// exercise 1-3
-
 #include <stdio.h>
 
 int main(void) {
-    float fahr, celsius; // declaration
-    int lower, upper, step;
-
-    lower = 0; // definition
-    upper = 300;
-    step = 20;
-
-    fahr = lower; // expression statement
-    while (fahr <= upper) {
-        celsius = 5.0 / 9.0 * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    printf("Hello, World!\n");
+    return 0;
 }
