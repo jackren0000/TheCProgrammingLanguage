629719d1328e0d43eadcd607b2699e5b787edf17
jackren <sren0023@student.monash.edu>
2024-10-27 17:40:23 +1100

    exercise 1-7

---
+++ exercise_1-7.c
@@ -0,0 +1,8 @@
+//The unary operators * and & bind more tightly than arithmetic operators.
+//Thus, *p+1 means 'take the object that p points to, and add 1 to it.auto
+
+#include <stdio.h>
+
+int main() {
+    printf("The value of EOF is %d", EOF);
+}
